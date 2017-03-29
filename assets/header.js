c29ce8206f838cb237bd0814fc96074b77828ae5
guillaumegenthial <genthial.guillaume@gmail.com>
2017-03-29 10:09:41 +0200

    added class togle on body click

--- assets/header.js
+++ assets/header.js
@@ -1,9 +1,16 @@
-function myFunction() {
+function changeClass() {
     element = document.getElementById("trigger");
     if (element.className == "trigger"){
         element.className = "show";
     }
     else {
+        element.className = "trigger";
+    }
+}
+
+function hideClass() {
+    element = document.getElementById("trigger");
+    if (element.className == "show") {
         element.className = "trigger";
     }
 }
